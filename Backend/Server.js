e1743e6c36d68240621aded9e55764b0dcd6f115
hunter007861 <sarveshbhosale111@gmial.com>
2024-10-05 21:21:13 +0100

    backend setup

---
+++ Backend/Server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+//app config
+const app = express();
+const port = process.env.PORT || 5000;
+const cookieParser = require("cookie-parser");
+
+//middleware
+app.use(cookieParser());
+//app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var corsOptions = {
+  methods: "GET, PUT, POST, DELETE, PATCH",
+  credentials: true,
+  origin: "http://localhost:5173",
+};
+
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+
+  next();
+};
+app.use(allowCrossDomain);
+app.options("*", cors());
+app.use(cors(corsOptions));
+
+//routes
+app.get("/", (req, res) => res.status(200).send("Nasa API webserver"));
+
+//Listening
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
