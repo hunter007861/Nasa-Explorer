80bff4385c4489ea102bb452fe3486ccfd7a5c63
hunter007861 <sarveshbhosale111@gmial.com>
2024-10-06 21:34:09 +0100

    backend deploy

--- Frontend/NASA-Explorer/src/components/Topbar.jsx
+++ Frontend/NASA-Explorer/src/components/Topbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import RocketIcon from '@mui/icons-material/Rocket';
+import RocketIcon from "@mui/icons-material/Rocket";
+import { useNavigate } from "react-router-dom";
 const Topbar = () => {
+  const navigate = useNavigate();
   return (
     <header className="flex items-center justify-between border-b border-solid border-b-[#242a47] px-10 py-3">
-      <div className="flex gap-4 text-white">
-        <div className="size-4">
-          <RocketIcon  className="mb-1"/>
+      <div
+        className="flex gap-4 text-white cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        <div className="size-4 ">
+          <RocketIcon className="mb-1" />
         </div>
         <h2 className="text-lg font-bold">NASA Explore</h2>
       </div>
--- Frontend/NASA-Explorer/src/config/AxiosConfig.js
+++ Frontend/NASA-Explorer/src/config/AxiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000";
+const baseURL = "https://nasa-explorer-backend-xed6.onrender.com";
 
 const Axios = axios.create({
     baseURL: baseURL,
